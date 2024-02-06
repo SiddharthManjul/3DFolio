9f2befb37b8036cb0e8e971cc68aa981ba00e97e
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-02-06 18:07:26 +0530

    File System

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+
+
+const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <h1>Nav</h1>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navigation;
